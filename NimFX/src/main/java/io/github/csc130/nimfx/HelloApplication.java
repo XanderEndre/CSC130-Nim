b40840f885b585a0e5291d7a9a9571958a1e39d9
CynthiaLlamas <98438989+CynthiaLlamas@users.noreply.github.com>
2023-01-18 13:36:14 -0700

    Test Commit

--- NimFX/src/main/java/io/github/csc130/nimfx/HelloApplication.java
+++ NimFX/src/main/java/io/github/csc130/nimfx/HelloApplication.java
@@ -2,18 +2,23 @@ package io.github.csc130.nimfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
+        //NimGame nimGame = new NimGame();
         stage.setScene(scene);
+        //nimGame.start(stage);
         stage.show();
     }
 
--- NimFX/src/main/java/io/github/csc130/nimfx/NimGame.java
+++ NimFX/src/main/java/io/github/csc130/nimfx/NimGame.java
@@ -1,13 +1,47 @@
 package io.github.csc130.nimfx;
 
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
 
-public class NimGame {
+public class NimGame extends Application {
     @FXML
-    Canvas cvsItemsCon;
+    Canvas cvsItemsCon = new Canvas();
     public void drawLines(GraphicsContext gc,int numOfLines){
-        gc.fillOval(10,15,30,15);
+        int x1 = 10;
+        int y1 = 15;
+        int x2 = 30;
+        int y2 = 15;
+        for (int shape = 0; shape < numOfLines; shape++) {
+            gc.strokeLine(x1,y1,x2,y2);
+            x1 += 10;
+            x2 = x1*2;
+        }
+
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group groupObject = new Group();
+        GraphicsContext gc = cvsItemsCon.getGraphicsContext2D();
+        int totalLines = 9;
+        int x1 = 10;
+        int y1 = 15;
+        int x2 = 30;
+        int y2 = 15;
+        gc.setLineWidth(5);
+        for (int shapes = 0; shapes < totalLines; shapes++) {
+            gc.strokeLine(x1,y1,x2,y2);
+            x1 += 10;
+            x2 = x1*2;
+        }
+        groupObject.getChildren().add(cvsItemsCon);
+        Scene sceneObj = new Scene(groupObject);
+        stage.setScene(sceneObj);
     }
 }
