8866e0d0348a00ed4c08a07bbbf5c1cacdc55b23
RGonzalez03 <rgonzalez@student.neumont.edu>
2023-01-20 09:38:21 -0700

    Basic setup for retry menu

--- NimFX/src/main/java/io/github/csc160/controller/RetryController.java
+++ NimFX/src/main/java/io/github/csc160/controller/RetryController.java
@@ -1,4 +1,24 @@
 package io.github.csc160.controller;
 
-public class RetryController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RetryController implements Initializable {
+    @FXML
+    Button btnRetry;
+    @FXML
+    Button btnQuit;
+//    will return the user to the main menu if clicked,doesn't end the program like "quit" does
+    @FXML
+    Button btnReturnToMenu;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
